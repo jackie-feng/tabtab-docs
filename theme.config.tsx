d1baae5524a5c5917c18020ea5fd5986ddd81cb9
jackie.feng <jackie.feng777@gmail.com>
2025-04-13 15:18:35 +0800

    GA

--- theme.config.tsx
+++ theme.config.tsx
@@ -14,8 +14,22 @@ const config: DocsThemeConfig = {
         name="description"
         content="TabTab 是一个管理浏览器标签页的插件。它可以帮助您管理多个标签页，并提供多种功能，如标签页分组、标签页搜索、标签页管理等。"
       />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-RB34Q1JCEZ"
+      ></script>
+      <script>
+        {`
+     window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-RB34Q1JCEZ');
+    `}
+      </script>
     </>
   ),
+
   logo: <b>TabTab</b>,
   logoLink: "https://tabtab.xyz",
   project: {
